6853967a3d20dbf6837ec199fedaea318b1a938d
Felipe Angelo <fangeloferreira2013@gmail.com>
2020-06-12 12:33:50 -0300

    12/06/2020-12:31-Ended partialy the class 11.
    Ended partialy the class 11.

---
+++ Aula-11/ex0009.js
@@ -0,0 +1,11 @@
+var country = 'Brasil';
+if(country == 'Brasil'){
+    console.log('Você é brasileiro!');
+}else{
+    console.log('Você é estrangeiro!');
+}
+if(country != 'Brasil'){
+    console.log('Você é estrangeiro!');
+}else{
+    console.log('Você é brasileiro!');
+}
